967ec97a44c6500d8e5029b4354a03d1a487fabc
Benjamin Tapia Arana <66929251+Benja-Tapia-Arana@users.noreply.github.com>
2023-04-03 18:47:23 -0400

    Mejora de estructura según programación funcional; se añadio recursividad a cambio de un ciclo for y se añadio una función auxiliar para generar outputs

--- fibonacci.js
+++ fibonacci.js
@@ -1,19 +1,15 @@
+function function_results(x, y) {return console.log(`F(${x}) = ${y}\n`);}
+
 function fibonacci_sequence(number) {
-    var n0 = 0;
-    var n1 = 0;
-    var n2 = 1;
-    if (number <= 1) {
-        if(number == 1) {return console.log(`F(${n2}) = ${n2}\n`);}
-        else {return console.log(`F(${n1}) = ${n1}\n`);}
-    }
-    else {
-        for (var i = 0; i < number; i++) {
-            var v = n2;
-            n2 = n1 + n2;
-            n1 = v;
-            n0++;
+    const fibonacci = (n0, n1 = 0, n2 = 1) => {
+        if (n0 <= 0) {
+            function_results(number, n1);
+            return;
         }
-        return console.log(`F(${n0}) = ${n1}`);
+        else {return fibonacci (n0 - 1, n2, n1 + n2);}
     }
+    fibonacci(number);
+    return;
 }
-module.exports.sequence = fibonacci_sequence;
+
+module.exports.sequence = fibonacci_sequence; 
